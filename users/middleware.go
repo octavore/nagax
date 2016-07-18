cc14f36a6f1569cbd3ba7b47ad1a79566d807330
tp <tp@octavore.com>
2016-07-17 21:10:21 -0700

    users: MustAuthenticateFunc

--- users/middleware.go
+++ users/middleware.go
@@ -7,18 +7,28 @@ import (
 
 type UserTokenKey struct{}
 
-func (m *Module) AuthenticatedFunc(next http.HandlerFunc) http.HandlerFunc {
+func (m *Module) authenticatedFunc(next, unauthenticatedNext http.HandlerFunc) http.HandlerFunc {
 	return func(rw http.ResponseWriter, req *http.Request) {
 		userToken, err := m.Authenticate(req)
 		if err != nil {
-			http.Redirect(rw, req, "/", http.StatusForbidden)
-			return
+			unauthenticatedNext(rw, req)
+		} else {
+			ctx := context.WithValue(req.Context(), UserTokenKey{}, userToken)
+			next(rw, req.WithContext(ctx))
 		}
-		ctx := context.WithValue(req.Context(), UserTokenKey{}, userToken)
-		next(rw, req.WithContext(ctx))
 	}
 }
 
-func (m *Module) AuthenticatedHandler(next http.Handler) http.HandlerFunc {
-	return m.AuthenticatedFunc(next.ServeHTTP)
+func (m *Module) AuthenticatedFunc(next http.HandlerFunc) http.HandlerFunc {
+	return m.authenticatedFunc(next, next)
+}
+
+func (m *Module) MustAuthenticateFunc(next http.HandlerFunc) http.HandlerFunc {
+	return m.authenticatedFunc(next, func(rw http.ResponseWriter, req *http.Request) {
+		http.Redirect(rw, req, "/", http.StatusForbidden)
+	})
+}
+
+func (m *Module) MustAuthenticateHandler(next http.Handler) http.HandlerFunc {
+	return m.MustAuthenticateFunc(next.ServeHTTP)
 }
