d47337dcc9075f7630254b1f3eece66e01f368be
tp <tp@octavore.com>
2017-09-09 23:41:38 -0700

    middleware: Update to use new router API.

--- users/middleware.go
+++ users/middleware.go
@@ -4,49 +4,49 @@ import (
 	"context"
 	"errors"
 	"net/http"
+
+	"github.com/octavore/nagax/router"
 )
 
 type UserTokenKey struct{}
 
-func (m *Module) WithAuth(next http.HandlerFunc) http.HandlerFunc {
+func (m *Module) WithAuth(next router.Handle) router.Handle {
 	return m.WithAuthList(m.Authenticators, next)
 }
 
-func (m *Module) MustWithAuth(next http.HandlerFunc) http.HandlerFunc {
+func (m *Module) MustWithAuth(next router.Handle) router.Handle {
 	return m.MustWithAuthList(m.Authenticators, next)
 }
 
-func (m *Module) WithAuthList(authenticators []Authenticator, next http.HandlerFunc) http.HandlerFunc {
-	return func(rw http.ResponseWriter, req *http.Request) {
+func (m *Module) WithAuthList(authenticators []Authenticator, next router.Handle) router.Handle {
+	return func(rw http.ResponseWriter, req *http.Request, par router.Params) error {
 		_, userToken, err := m.authenticate(authenticators, rw, req)
 		if err != nil {
-			m.ErrorHandler(rw, req, err)
-			return
+			return err
 		}
 		if userToken != nil {
 			ctx := context.WithValue(req.Context(), UserTokenKey{}, *userToken)
 			req = req.WithContext(ctx)
 		}
-		next(rw, req)
+		return next(rw, req, par)
 	}
 }
 
-var ErrNotAuthorized = errors.New("not authorized")
+var ErrNotAuthorized = errors.New("not authorized") // todo: make this better
 
-func (m *Module) MustWithAuthList(authenticators []Authenticator, next http.HandlerFunc) http.HandlerFunc {
-	return func(rw http.ResponseWriter, req *http.Request) {
+func (m *Module) MustWithAuthList(authenticators []Authenticator, next router.Handle) router.Handle {
+	return func(rw http.ResponseWriter, req *http.Request, par router.Params) error {
 		handled, userToken, err := m.authenticate(authenticators, rw, req)
 		if err != nil {
-			m.ErrorHandler(rw, req, err)
-			return
+			return err
 		}
 
 		if !handled || userToken == nil {
 			m.ErrorHandler(rw, req, ErrNotAuthorized)
-			return
+			return ErrNotAuthorized
 		}
 
 		ctx := context.WithValue(req.Context(), UserTokenKey{}, *userToken)
-		next(rw, req.WithContext(ctx))
+		return next(rw, req.WithContext(ctx), par)
 	}
 }
