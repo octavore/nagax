053008f0f345c97a1404229f0d78997838190ed1
tp <tp@octavore.com>
2017-11-13 22:08:42 -0800

    csrf: Change implementation of middleware.

--- router/middleware/csrf/csrf.go
+++ router/middleware/csrf/csrf.go
@@ -12,6 +12,13 @@ import (
 	"github.com/octavore/nagax/util/errors"
 )
 
+var csrfWhitelist = map[string]bool{
+	"GET":     true,
+	"HEAD":    true,
+	"OPTIONS": true,
+	"TRACE":   true,
+}
+
 type Module struct {
 	Router  *router.Module
 	Session *session.Module
@@ -24,31 +31,27 @@ func (m *Module) Init(c *service.Config) {
 
 func (m *Module) New() func(rw http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
 	return func(rw http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
-		session, err := m.Session.Verify(req)
-		if err != nil {
-			// bad request cookie
-			m.Logger.Warning(errors.Wrap(err))
-			re := router.NewRequestError(req, http.StatusBadRequest, "invalid request")
-			m.Router.HandleError(rw, req, re)
-			return
-		}
-
-		csrfHeader := req.Header.Get("x-csrf-token")
-		ok, err := m.CSRF.Verify(session, csrfHeader)
-		if err != nil {
-			// error decoding the token
-			m.Logger.Warning(errors.Wrap(err))
-			re := router.NewRequestError(req, http.StatusBadRequest, "invalid request")
-			m.Router.HandleError(rw, req, re)
-			return
-		}
-
-		if !ok {
-			// invalid token
-			m.Logger.Warning(errors.New("invalid csrf token"))
-			re := router.NewRequestError(req, http.StatusBadRequest, "invalid csrf token")
-			m.Router.HandleError(rw, req, re)
-			return
+		if !csrfWhitelist[req.Method] {
+			session, err := m.Session.Verify(req)
+			if err != nil {
+				m.Router.HandleError(rw, req, errors.Wrap(err))
+				return
+			}
+			// only check csrf if user is logged in
+			// don't check csrf for logged out users right now
+			if session != "" {
+				csrfToken := req.Header.Get("x-csrf-token")
+				ok, err := m.CSRF.Verify(session, csrfToken)
+				if err != nil {
+					m.Router.HandleError(rw, req, errors.Wrap(err))
+					return
+				}
+				if !ok {
+					err := router.NewRequestError(req, http.StatusBadRequest, "invalid csrf token")
+					m.Router.HandleError(rw, req, errors.Wrap(err))
+					return
+				}
+			}
 		}
 		next(rw, req)
 	}
