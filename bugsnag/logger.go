168df3442ae5ff682d4abb23f91f7dfc0476bb4c
tp <tp@octavore.com>
2018-02-25 15:30:47 -0800

    bugsnag: Initial commit of bugsnag module.

---
+++ bugsnag/logger.go
@@ -0,0 +1,47 @@
+package bugsnag
+
+import (
+	"fmt"
+	"net/http"
+
+	goerrors "github.com/go-errors/errors"
+
+	"github.com/octavore/nagax/logger"
+	"github.com/octavore/nagax/util/errors"
+)
+
+type bugsnagLogger struct {
+	logger.Logger
+	Notify func(error, ...interface{}) // Notify is the module.Notify which wraps bugsnag.Notify with added print
+}
+
+func (b *bugsnagLogger) Error(args ...interface{}) {
+	if len(args) == 1 {
+		if originalErr, ok := args[0].(error); ok {
+			// this handles case with only one error arugment
+			var req *http.Request
+			if re, ok := originalErr.(GetRequestable); ok {
+				req = re.GetRequest()
+			}
+			if err, ok := originalErr.(*goerrors.Error); ok {
+				if re, ok := err.Err.(GetRequestable); ok && re.GetRequest() != nil {
+					req = re.GetRequest()
+				}
+			} else {
+				originalErr = errors.WrapS(originalErr, 1)
+			}
+			if req != nil {
+				b.Notify(originalErr, req)
+			} else {
+				b.Notify(originalErr)
+			}
+			return
+		}
+	}
+	b.Notify(errors.New(fmt.Sprint(args...)))
+}
+
+func (b *bugsnagLogger) Errorf(format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	b.Error(msg)
+}
---
+++ bugsnag/module.go
@@ -0,0 +1,105 @@
+/*
+bugsnag module configures bugsnag and modifies the logger so logger.Error logs to bugsnag as well.
+*/
+package bugsnag
+
+import (
+	"fmt"
+	"net/http"
+
+	bugsnagGo "github.com/bugsnag/bugsnag-go"
+	goerrors "github.com/go-errors/errors"
+	"github.com/octavore/naga/service"
+
+	"github.com/octavore/nagax/config"
+	"github.com/octavore/nagax/logger"
+	"github.com/octavore/nagax/util/errors"
+)
+
+type Config struct {
+	Bugsnag *struct {
+		APIKey string `json:"api_key"`
+	} `json:"bugsnag"`
+}
+
+type Module struct {
+	Logger *logger.Module
+	Config *config.Module
+
+	config         Config
+	bugsnagEnabled bool
+	originalErrorf func(format string, args ...interface{})
+}
+
+var _ service.Module = &Module{}
+
+// AppVersion is set via a build flag
+var AppVersion = ""
+
+func (m *Module) Init(c *service.Config) {
+	c.Setup = func() error {
+		err := m.Config.ReadConfig(&m.config)
+		if err != nil {
+			return err
+		}
+		m.originalErrorf = m.Logger.Logger.Errorf
+		if m.config.Bugsnag != nil {
+			m.Logger.Info("bugsnag enabled: ", AppVersion)
+			// note: this forces the app to restart
+			bugsnagGo.Configure(bugsnagGo.Configuration{
+				ReleaseStage: c.Env().String(),
+				ProjectPackages: []string{
+					"github.com/octavore/*",
+				},
+				APIKey:     m.config.Bugsnag.APIKey,
+				Logger:     Printfer(m.Logger.Infof), // may be redundant with our own logging
+				AppVersion: AppVersion,
+			})
+			m.bugsnagEnabled = true
+		}
+
+		m.Logger.Logger = &bugsnagLogger{Logger: m.Logger.Logger, Notify: m.Notify}
+		return nil
+	}
+}
+
+type Printfer func(fmt string, args ...interface{})
+
+func (p Printfer) Printf(fmt string, args ...interface{}) {
+	p(fmt, args...)
+}
+
+type GetRequestable interface {
+	GetRequest() *http.Request
+}
+
+type ErrorStackable interface {
+	ErrorStack() string
+}
+
+// Notify bugsnag, note that m.Logger.Error calls Notify so Notify musn't call m.Logger.Error
+func (m *Module) Notify(err error, rawData ...interface{}) {
+	rawData = append(rawData, bugsnagGo.SeverityError)
+	if re, ok := err.(GetRequestable); ok && re.GetRequest() != nil {
+		rawData = append(rawData, re.GetRequest())
+	}
+	errType := "error"
+	if err2, ok := err.(*goerrors.Error); ok {
+		errType = fmt.Sprintf("%T", err2.Err)
+		rawData = append(rawData, bugsnagGo.ErrorClass{Name: errType})
+	}
+
+	if !m.bugsnagEnabled {
+		if re, ok := err.(ErrorStackable); ok {
+			m.originalErrorf("bugsnag fake: %T %v %#v\n%s", errType, err, rawData, re.ErrorStack())
+		} else {
+			m.originalErrorf("bugsnag fake: %T %v %#v", errType, err, rawData)
+		}
+		return
+	}
+	e := bugsnagGo.Notify(err, rawData...)
+	if e != nil {
+		m.originalErrorf("%v", errors.WrapS(e, 2))
+	}
+	// note: bugsnag does its own logging
+}
