d38c71d692c17410aecfdc6ae8bd685af0ceea5c
tp <tp@octavore.com>
2019-08-07 12:01:06 -0700

    csrf: Rework to be more flexible about ignoring paths.

--- router/middleware/csrf/csrf.go
+++ router/middleware/csrf/csrf.go
@@ -2,7 +2,9 @@ package csrf
 
 import (
 	"net/http"
+	"strings"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/octavore/naga/service"
 
 	"github.com/octavore/nagax/logger"
@@ -11,6 +13,8 @@ import (
 	"github.com/octavore/nagax/users/session"
 	"github.com/octavore/nagax/util/errors"
 )
+
+const csrfHeaderKey = "x-csrf-token"
 
 var csrfWhitelist = map[string]bool{
 	"GET":     true,
@@ -30,41 +34,58 @@ func (m *Module) Init(c *service.Config) {
 }
 
 func (m *Module) New(ignorePaths ...string) func(rw http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
-	ignoreMap := map[string]bool{}
-	for _, url := range ignorePaths {
-		ignoreMap[url] = true
+	ignoreRouter := httprouter.New()
+	noopHandler := func(http.ResponseWriter, *http.Request, httprouter.Params) {}
+	for _, path := range ignorePaths {
+		ignoreRouter.POST(strings.TrimSuffix(path, "/"), noopHandler)
 	}
 
 	return func(rw http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
-		if !csrfWhitelist[req.Method] {
-			session, err := m.Session.Verify(req)
-			if err != nil {
-				m.Router.HandleError(rw, req, errors.Wrap(err))
-				return
-			}
-			// only check csrf if user is logged in
-			// don't check csrf for logged out users right now
-			if session != "" && !ignoreMap[req.URL.Path] {
-				csrfToken := req.Header.Get("x-csrf-token")
-				var ok bool
-				if csrfToken == "" && req.Method == "POST" {
-					csrfToken = req.PostFormValue("csrfToken")
-				}
-				if csrfToken == "" {
-					ok = false
-				} else {
-					ok, err = m.CSRF.Verify(session, csrfToken)
-					if err != nil {
-						ok = false
-						m.Logger.Error(errors.Wrap(err))
-					}
-				}
-				if !ok {
-					err := router.NewRequestError(req, http.StatusBadRequest, "invalid csrf token")
-					m.Router.HandleError(rw, req, errors.Wrap(err))
-					return
-				}
-			}
+		// 1. whitelisted: ignore csrf
+		if csrfWhitelist[req.Method] {
+			next(rw, req)
+			return
+		}
+
+		// 2. h != nil: this path is explicitly not checked for csrf
+		cleanedPath := strings.TrimSuffix(req.URL.Path, "/")
+		h, _, _ := ignoreRouter.Lookup("POST", cleanedPath)
+		if h != nil {
+			next(rw, req)
+			return
+		}
+
+		// 3. only check csrf if user is logged in
+		// don't check csrf for logged out users right now
+		session, err := m.Session.Verify(req)
+		if err != nil {
+			m.Router.HandleError(rw, req, errors.Wrap(err))
+			return
+		}
+		if session == "" {
+			next(rw, req)
+			return
+		}
+
+		// 4. csrf token is required, so check it
+		csrfToken := req.Header.Get(csrfHeaderKey)
+		if csrfToken == "" && req.Method == "POST" {
+			csrfToken = req.PostFormValue("csrfToken")
+		}
+		if csrfToken == "" {
+			err := router.NewRequestError(req, http.StatusBadRequest, "csrf token missing")
+			m.Router.HandleError(rw, req, errors.Wrap(err))
+			return
+		}
+		ok, err := m.CSRF.Verify(session, csrfToken)
+		if err != nil {
+			ok = false
+			m.Logger.Error(errors.Wrap(err))
+		}
+		if !ok {
+			err := router.NewRequestError(req, http.StatusBadRequest, "invalid csrf token")
+			m.Router.HandleError(rw, req, errors.Wrap(err))
+			return
 		}
 		next(rw, req)
 	}
---
+++ router/middleware/csrf/csrf_test.go
@@ -0,0 +1,79 @@
+package csrf
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/octavore/naga/service"
+	"github.com/octavore/nagax/users/session"
+	"github.com/octavore/nagax/util/memlogger"
+)
+
+type TestModule struct {
+	*Module
+}
+
+func (m *TestModule) Init(c *service.Config) {
+	c.Setup = func() error {
+		m.Logger.Logger = &memlogger.MemoryLogger{}
+		return nil
+	}
+}
+
+type testEnv struct {
+	module *Module
+	logger *memlogger.MemoryLogger
+	stop   func()
+}
+
+func setup() testEnv {
+	module := &TestModule{}
+	stop := service.New(module).StartForTest()
+	return testEnv{
+		module: module.Module,
+		logger: module.Logger.Logger.(*memlogger.MemoryLogger),
+		stop:   stop,
+	}
+}
+
+func TestNew(t *testing.T) {
+	env := setup()
+	defer env.stop()
+
+	testHandler := env.module.New("/ignore", "/ignore2/:id")
+	testPaths := []struct {
+		path   string
+		method string
+		valid  bool
+	}{
+		{path: "/", method: "POST", valid: false},                // INVALID
+		{path: "/path", method: "POST", valid: false},            // INVALID
+		{path: "/path", method: "GET", valid: true},              // -> valid because method is ignored
+		{path: "/ignore", method: "POST", valid: true},           // -> valid because ignored
+		{path: "/ignore2", method: "POST", valid: false},         // INVALID
+		{path: "/ignore2/foo", method: "POST", valid: true},      // -> valid because it matches pattern
+		{path: "/ignore2/foo/sub", method: "POST", valid: false}, // INVALID
+	}
+
+	session, err := env.module.Session.NewSessionCookie(&session.UserSession{ID: "1", SessionID: "fake"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, c := range testPaths {
+		req := httptest.NewRequest(c.method, c.path, nil)
+		req.AddCookie(session)
+		// req.Header.Set(csrfHeaderKey, "fake")
+		rr := httptest.NewRecorder()
+		testHandler(rr, req, func(rw http.ResponseWriter, req *http.Request) {})
+		if c.valid {
+			assert.Equal(t, rr.Code, 200, fmt.Sprintf("%s %s should be valid", c.method, c.path))
+		} else {
+			assert.Equal(t, rr.Code, 400, fmt.Sprintf("%s %s should be invalid", c.method, c.path))
+		}
+	}
+
+}
