48466f554e29e67e303ac6ca9b2b89b9146a3974
tp <tp@octavore.com>
2023-07-10 21:57:57 -0400

    errors: Simplify lib.

--- util/errors/errors.go
+++ util/errors/errors.go
@@ -1,52 +1,43 @@
 package errors
 
 import (
+	"errors"
 	"fmt"
-	"reflect"
 
-	"github.com/go-errors/errors"
+	goerrors "github.com/go-errors/errors"
 )
 
 // New returns a new wrapped error with m as message
 func New(m string, a ...interface{}) error {
-	return errors.Wrap(fmt.Errorf(m, a...), 1)
+	return goerrors.Wrap(fmt.Errorf(m, a...), 1)
 }
 
 // Wrap an error e if it is not nil
-func Wrap(e error) error {
-	if e == nil {
-		return nil
+func Wrap(err error) error {
+	var e *goerrors.Error
+	if errors.As(err, &e) {
+		return err
 	}
-	if _, ok := e.(*errors.Error); ok {
-		return e
-	}
-	return errors.Wrap(e, 1)
+	return goerrors.Wrap(err, 1)
 }
 
 // WrapS is like Wrap but skips 'skip' lines of trace
-func WrapS(e error, skip int) error {
-	if e == nil {
-		return nil
+func WrapS(err error, skip int) error {
+	var e *goerrors.Error
+	if errors.As(err, &e) {
+		return err
 	}
-	if _, ok := e.(*errors.Error); ok {
-		return e
-	}
-	return errors.Wrap(e, skip+1)
+	return goerrors.Wrap(err, skip+1)
 }
 
 func IsType(e1, e2 error) bool {
-	if err, ok := e1.(*errors.Error); ok {
-		return IsType(err.Err, e2)
-	}
-	if err, ok := e2.(*errors.Error); ok {
-		return IsType(e1, err.Err)
-	}
-	return reflect.TypeOf(e1) == reflect.TypeOf(e2)
+	return errors.Is(e1, e2) || errors.Is(e2, e1)
 }
 
-func TypeName(e error) string {
-	if err, ok := e.(*errors.Error); ok {
-		return err.TypeName()
+func TypeName(err error) string {
+	var e *goerrors.Error
+	if errors.As(err, &e) {
+		return e.TypeName()
 	}
-	return fmt.Sprintf("%T", e)
+	return fmt.Sprintf("%T", err)
 }
